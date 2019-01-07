34e4cc564501ea094922525b8c473d280f43de87
mfbradley <mbradleystylist@gmail.com>
2019-01-07 16:31:17 -0500

    created pages folder with About page skeleton

---
+++ client/src/Pages/About/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./About.css";
+
+class About extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="About">
+
+            </div>
+        )
+    }
+}
+
+export default About;
