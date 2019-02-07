f2dfa7bfc1f404d0081618bc3a0f6175725c44bd
mfbradley <mbradleystylist@gmail.com>
2019-02-07 13:16:22 -0500

    adding Typewriter component folder and files

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -25,10 +25,10 @@ class Home extends React.Component {
     //     }));
 
     // }
-
+   
 
     render() {
-
+        
         return (
             <div>
                 <div className="Home">
