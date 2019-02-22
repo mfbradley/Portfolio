4030e023cda46f383fe9ded28ede4c1f59ea7489
mfbradley <mbradleystylist@gmail.com>
2019-02-22 14:01:21 -0500

    updated projectSquare to include title, languages, and links

--- client/src/Components/ProjectSquare/ProjectSquare.js
+++ client/src/Components/ProjectSquare/ProjectSquare.js
@@ -7,14 +7,16 @@ class ProjectSquare extends React.Component {
     }
 
     render() {
-        return(
+        return (
             <div className="project-square">
                 <p className="project-name">{this.props.projectText}</p>
                 <hr></hr>
                 <p className="project-languages">{this.props.languages}</p>
                 <hr></hr>
-                <a href={this.props.github} target="_blank"><i class="devicon-github-plain-wordmark"></i></a>
-                <a href={this.props.site} target="_blank"><i class="devicon-chrome-plain"></i></a>
+                <div className="project-links">
+                    <a href={this.props.github} target="_blank"><i class="devicon-github-plain-wordmark"></i></a>
+                    <a href={this.props.site} target="_blank"><i class="devicon-chrome-plain"></i></a>
+                </div>
             </div>
         )
     }
