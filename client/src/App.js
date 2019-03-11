164b6f26f7f993a9d4797bb9f08448966008bb8e
mfbradley <mbradleystylist@gmail.com>
2019-03-11 16:56:28 -0400

    updated App component declaration

--- client/src/App.js
+++ client/src/App.js
@@ -1,54 +1,13 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-import { CSSTransitionGroup } from 'react-transition-group';
 import Home from './Pages/Home';
-import About from './Pages/About';
-import Portfolio from './Pages/Portfolio/';
-import Contact from './Pages/Contact'
-
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-  }
-
-  render() {
+function App() {
 
     return (
-
-      // <Router>
-      //   <div className="App">
-
-      //     <Route render={({ location }) => (
-
-      //       < CSSTransitionGroup
-      //         transitionName="fade"
-      //         transitionEnterTimeout={2000}
-      //         transitionLeave={false}
-      //         transitionLeaveTimeout={2000}
-      //       >
-
-      //         <Switch key={location.key} location={location}>
-      //           <Route exact path="/" component={Home} />
-      //           <Route exact path="/about" component={About} />
-      //           <Route exact path="/portfolio" component={Portfolio} />
-      //           <Route exact path="/contact" component={Contact} />
-      //           {/* <Route component={NoMatch} /> */}
-      //         </Switch>
-      //       </CSSTransitionGroup>
-      //     )}
-
-      //     />
-      //   </div>
-      // </Router>
-
       <Home />
-
     );
-  }
+
 }
 
 export default App;
